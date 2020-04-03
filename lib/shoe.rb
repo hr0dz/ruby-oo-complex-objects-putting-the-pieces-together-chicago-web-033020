2940cd27c82663a719f46b32d234eef36237e1e8
Haley Rodzos <haley.rodzos@gmail.com>
2020-04-03 02:09:00 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,7 +1,7 @@
 # Make your shoe class here!
 class Shoe
   attr_reader :brand, :shoe, :condition
-  attr_accessor :color, :size, :material, :condition
+  attr_accessor :color, :size, :material
  
   
   def initialize(brand)
