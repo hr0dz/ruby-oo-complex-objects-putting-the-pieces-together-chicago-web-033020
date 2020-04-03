61fdbe4d485c7f1d6f3b5553012e851839e2b6ed
Haley Rodzos <haley.rodzos@gmail.com>
2020-04-03 01:51:44 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,29 @@
 # Make your shoe class here!
+class Shoe
+  attr_reader :brand
+  attr_accessor :color, :size, :material, :condition
+ 
+  
+  def initialize(brand)
+   @brand = brand
+  end
+  
+  def brand=(brand)
+    @brand = brand
+  end
+  
+  def cobble(shoe)
+    if shoe.condition = "new"
+      return "Your shoe is as good as new!"
+    
+end
+ 
+end
+
+Shoe.new("Nike")
+color = Shoe.new("red")
+size = Shoe.new(9.5)
+material = Shoe.new("suede")
+condition = Shoe.new("tattered")
+condition = Shoe.new("new")
+ 
